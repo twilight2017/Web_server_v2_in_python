e9f40539ca1a52c462d6291c6625db09137ae276
twilight2017 <liuzixuanp@163.com>
2022-03-14 19:18:07 +0800

    add assignment volume function

--- statistics.py
+++ statistics.py
@@ -15,7 +15,7 @@ worksheet = workbook.add_worksheet()
 bold = workbook.add_format({'bold': 1})
 
 # 准备数据写入excel中
-headings = ['Period', '上周', '本周']
+headings = ['Name', '上周', '本周']
 data = [
     ['罗远明', '刘兴国', '王超', '王言章', '李若愚', '聂正']
 ]
@@ -23,12 +23,12 @@ data = [
 
 # 代码量统计
 def code_count():
-    data_code_list = data
+    data_code_list = data.copy()
     # 上周代码量数据
     list_last_week = [795, 1789, 357, 653, 14438, 506]
     list_this_week = [1431, 2228, 1686, 1849, 14619, 531]
     data_code_list += list_last_week, list_this_week
-    #print(type(data_code_list[1][2]))
+    # print(type(data_code_list[1][2]))
     # 写入表头
     worksheet.write_row('A1', headings, bold)
     # 写入数据
@@ -68,9 +68,57 @@ def code_count():
 
     # 把图表插入worksheet以及偏移
     worksheet.insert_chart('A10', chart, {'X_offset': 25, 'y_sheet': 10})
+    #workbook.close()
+
+
+# 任务量统计
+def assignment_volumn():
+    assign_data_list = data.copy()
+    assign_last_week = [0, 1, 8, 2, 5, 9]
+    assign_this_week = [1, 0, 1, 3, 4, 0]
+    assign_data_list += assign_last_week, assign_this_week
+    print(assign_data_list)
+
+    # 写入表头
+    worksheet.write_row('J1', headings, bold)
+    # 写入数据
+    worksheet.write_column('J2', assign_data_list[0])
+    worksheet.write_column('K2', assign_data_list[1])
+    worksheet.write_column('L2', assign_data_list[2])
+
+    # 生成图表并插入到excel
+    # 1.创建一个柱状图
+    chart_2 = workbook.add_chart({'type': 'column'})
+    # 2.配置第一个系列的数据
+    chart_2.add_series({
+        'name': '=Sheet1!$K$1',
+        'categories': '=Sheet1!$J2:$J7',
+        'values': '=Sheet1!$K2:$K7',
+        'line': {'color': 'yellow'},
+    })
+
+    # 3.配置第二个系列数据
+    chart_2.add_series({
+        'name': '=Sheet1!$L$1',
+        'categories': '=Sheet1!$J2:$J7',
+        'values': '=Sheet1!$L2:$L7',
+        'line': {'color': 'purple'},
+    })
+
+    # 4.设置图表的title和x，y轴信息
+    chart_2.set_title({'name': '制造协同周任务量分析表'})
+    chart_2.set_x_axis({'name': 'Member name'})
+    chart_2.set_y_axis({'name': 'Weekly code volume data'})
+
+    # 设置图表的风格
+    chart_2.set_style(1)
+
+    # 把图表插入worksheet以及偏移
+    worksheet.insert_chart('J10', chart_2, {'X_offset': 25, 'y_sheet': 10})
     workbook.close()
 
 
 # 给出函数入口
 if __name__ == '__main__':
     code_count()
+    assignment_volumn()
